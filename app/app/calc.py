aa73412ea6c406a2fe6bc0f05898cb2270c141e6
Mahsa Sh <mahsa.zohdi96@gmail.com>
2023-11-04 16:34:49 +0330

    install postgresql database adaptor

---
+++ app/app/calc.py
@@ -0,0 +1,8 @@
+"""
+calculator function
+"""
+
+def add(x, y):
+    """ add x and y and return result. """
+    return x+y
+    
---
+++ app/app/tests.py
@@ -0,0 +1,16 @@
+"""
+simple tests
+
+"""
+
+from django.test import SimpleTestCase
+
+from app.calc import add
+
+class CalcTest(SimpleTestCase):
+    """ test test calc module. """
+
+    def test_add_numbers(self):
+        res = add(5,7)
+        self.assertEqual(res, 16)
+
